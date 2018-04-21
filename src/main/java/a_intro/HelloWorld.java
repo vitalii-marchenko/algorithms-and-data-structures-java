968eb9a886d9d4f89377689081c7ca66815d80e7
markony647 <markony647@gmail.com>
2018-04-21 15:57:40 +0300

    Add Hello World

---
+++ src/main/java/a_intro/HelloWorld.java
@@ -0,0 +1,11 @@
+package a_intro;
+
+public class HelloWorld {
+    private void run() {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        new HelloWorld().run();
+    }
+}
