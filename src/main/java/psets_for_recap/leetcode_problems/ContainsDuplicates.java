42606111bf02a27432037be0d099bea2cdd98027
Vitaly <markony647@gmail.com>
2018-05-08 10:34:38 +0300

    Add containsDuplicate pset solution

---
+++ src/main/java/psets_for_recap/leetcode_problems/ContainsDuplicates.java
@@ -0,0 +1,41 @@
+package psets_for_recap.leetcode_problems;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicates {
+    /*
+    Given an array of integers, find if the array contains any duplicates.
+
+    Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+    Example 1:
+
+    Input: [1,2,3,1]
+    Output: true
+    Example 2:
+
+    Input: [1,2,3,4]
+    Output: false
+    Example 3:
+
+    Input: [1,1,1,3,3,4,3,2,4,2]
+    Output: true
+     */
+
+    public boolean containsDuplicate(int[] nums) {
+        int originalLen = nums.length;
+        Set<Integer> mySet = new HashSet<>();
+        for (Integer num : nums) {
+            mySet.add(num);
+        }
+        return mySet.size() != originalLen;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,1,3,3,4,3,2,4,2};
+        int[] arr2 = {1,2,3,1};
+        new ContainsDuplicates().containsDuplicate(arr2);
+    }
+
+}
