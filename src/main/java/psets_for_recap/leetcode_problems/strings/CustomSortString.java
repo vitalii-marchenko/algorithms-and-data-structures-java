f1a831b02b99a6c8b4e439a53f5f8cb46c085512
Vitaly <markony647@gmail.com>
2018-05-18 10:37:13 +0300

    Add customSortString pset solution (medium level task)

--- src/main/java/psets_for_recap/leetcode_problems/strings/CustomSortString.java
+++ src/main/java/psets_for_recap/leetcode_problems/strings/CustomSortString.java
@@ -1,5 +1,11 @@
 package psets_for_recap.leetcode_problems.strings;
 
+import psets_for_recap.leetcode_problems.linked_lists.ListNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * S and T are strings composed of lowercase letters. In S, no letter occurs more than once.
  *
@@ -26,23 +32,42 @@ package psets_for_recap.leetcode_problems.strings;
 
 public class CustomSortString {
 
-    // NOT WORKING SOLUTION
     public String customSortString(String S, String T) {
-        char[] intersectionLetters = new char[T.length()];
-        StringBuilder toEnd = new StringBuilder();
-        for (char letter : T.toCharArray()) {
-            if (S.indexOf(letter) >= 0) {
-                int index = S.indexOf(letter);
+        char[] intersectionLetters = new char[S.length()];
+        char[] TLetters = T.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+        int index = 0;
+        for (char letter : S.toCharArray()) {
+            int letterIndex = T.indexOf(letter);
+            if (letterIndex >= 0) {
                 intersectionLetters[index] = letter;
-            } else {
-                toEnd.append(letter);
+                TLetters[letterIndex] = 0;
+                index++;
             }
         }
-        String s = convertArrToString(intersectionLetters);
-        return s + toEnd.toString();
+        List<Character> intersectionListChar = charArraytoList(intersectionLetters);
+        for (Character ch : TLetters) {
+            if (ch != 0) {
+                int i = intersectionListChar.indexOf(ch);
+                if (i >= 0) {
+                    intersectionListChar.add(i + 1, ch);
+                } else {
+                    intersectionListChar.add(ch);
+                }
+            }
+        }
+        return convertListToString(intersectionListChar) + stringBuilder.toString();
     }
 
-    private String convertArrToString(char[] arr) {
+    private List<Character> charArraytoList(char[] arr) {
+        List<Character> list = new ArrayList<>();
+        for (Character ch : arr) {
+            list.add(ch);
+        }
+        return list;
+    }
+
+    private String convertListToString(List<Character> arr) {
         StringBuilder stringBuilder = new StringBuilder();
         for (Character ch : arr) {
             if (ch != 0) {
@@ -53,6 +78,7 @@ public class CustomSortString {
     }
 
     public static void main(String[] args) {
+        new CustomSortString().customSortString("cba", "abcd"); // expected "cbad"
         new CustomSortString().customSortString("kqep", "pekeq"); // expected "kqeep"
 
     }
